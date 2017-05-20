6b5ab7fc1b439f23bbf437208e811ad1da952b8d
Ezequiel Pereira <ezequielsp@users.noreply.github.com>
2017-05-20 17:24:44 -0300

    Synfony files

---
+++ README.md
@@ -0,0 +1,4 @@
+sim
+===
+
+A Symfony project created on May 20, 2017, 10:24 am.
---
+++ src/Admin/TestBundle/AdminTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Admin\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdminTestBundle extends Bundle
+{
+}
