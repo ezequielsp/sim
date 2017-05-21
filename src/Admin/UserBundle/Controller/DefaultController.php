83c90688463fd573f1b7b0c4b635fd8cf15a23ee
Ezequiel Pereira <ezequielsp@users.noreply.github.com>
2017-05-20 22:30:16 -0300

    vimeo player

--- src/Admin/UserBundle/Controller/DefaultController.php
+++ src/Admin/UserBundle/Controller/DefaultController.php
@@ -12,6 +12,43 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('AdminUserBundle:Default:index.html.twig');
+        $oembed_endpoint = 'http://vimeo.com/api/oembed';
+
+        // Grab the video url from the url, or use default
+        $video_url = (isset($_GET['url'])) ? $_GET['url'] : 'http://vimeo.com/7100569';
+
+        // Create the URLs
+        $json_url = $oembed_endpoint . '.json?url=' . rawurlencode($video_url) . '&width=640';
+        $xml_url = $oembed_endpoint . '.xml?url=' . rawurlencode($video_url) . '&width=640';
+
+        // Load in the oEmbed XML
+        $oembed = simplexml_load_string($this->curlGet($xml_url));
+
+        $title       = $oembed->title;
+        $author_name = $oembed->author_name;
+        $author_url  = $oembed->author_url;
+        $video       = html_entity_decode($oembed->html);
+
+        return $this->render(
+            'AdminUserBundle:Default:index.html.twig',
+            [
+                'title'       => $title,
+                'author_name' => $author_name,
+                'author_url'  => $author_url,
+                'video'       => $video
+            ]
+        );
+    }
+
+    // Curl helper function
+    public function curlGet($url)
+    {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        $return = curl_exec($curl);
+        curl_close($curl);
+        return $return;
     }
 }
